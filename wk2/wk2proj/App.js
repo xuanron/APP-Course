f91b7d2482229721302fdd5fa9cd69e1cd93d795
xuanron <liyang20010820@gmail.com>
2022-03-01 19:13:05 +0800

    new

--- wk2/wk2proj/App.js
+++ wk2/wk2proj/App.js
@@ -1,7 +1,7 @@
-import React from "./node_modules/react";
-import { StyleSheet, Text, View, SafeAreaView, StatusBar } from 'react-native';
-import Header from './src/component/Header';
-import AlbumList from './src/component/AlbumList';
+import React from "react";
+import { StyleSheet, StatusBar, SafeAreaView } from "react-native";
+import Header from "./scr/component/Header";
+import AlbumList from "./scr/component/AlbumList";
 
 const App = () => {
   return (
@@ -13,12 +13,11 @@ const App = () => {
   );
 };
 
-
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "#1f1f1f",
-  }
+    backgroundColor:"black",
+  },
 });
 
 export default App;
